99d4a41ffe84f61491bc18b60303a0c9df3c21c6
austinschroeder <austinsoundy@gmail.com>
2021-07-30 16:02:10 -0700

    Succesully post to mongoDB

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://austin:Shelby5252@cluster0.ezy0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
