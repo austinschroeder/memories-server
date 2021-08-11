3c5b6ecc04c44686acf98cf485489c68e320e460
austinschroeder <austinsoundy@gmail.com>
2021-08-10 20:28:03 -0600

    Updated mongo IP security settings. Need to fix import error

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,10 @@
+import { mongoose } from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = async(req, res) => {
+export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    
+
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message })
@@ -24,4 +25,15 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message })
 
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+  res.json(updatedPost);
+
 }
